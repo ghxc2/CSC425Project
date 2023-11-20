c2d366c0d3d9262a9dfd096263eb2c7286190ac1
ghxc2 <42091882+ghxc2@users.noreply.github.com>
2023-11-19 22:16:39 -0500

    Added filter_wins()

--- AI_Player.py
+++ AI_Player.py
@@ -7,6 +7,9 @@ print(data.info())
 print()
 print(data.head())
 
+# LETTER FOR AI
+ai_letter = "O"
+
 
 def filter_chosen(board):
     """
@@ -30,10 +33,28 @@ def filter_chosen(board):
             # Check if spot has been claimed
             if spot != "":
 
+                if ai_letter == "O":
+                    if spot == "X":
+                        spot = "O"
+                    if spot == "O":
+                        spot = "X"
+
                 # filter out impossible moves
                 possible_moves = possible_moves.loc[data[data_col] == spot.lower()]
 
     return possible_moves
+
+
+def filter_wins(possible_moves):
+    """
+    filter_wins() takes an input of possible games moves, and filters out all win conditions possible
+    for future moves
+    :param possible_moves: possible future moves, best from filter_chosen()
+    :return: possible future wins the game can find
+    """
+    print(data["class"].loc[0])
+    wins = possible_moves.loc[data[data.columns[9]]]
+    return wins
 
 
 def search(board):
@@ -47,7 +68,9 @@ def search(board):
     # MAY BE MERGED WITH best_move()
     # should use data to determine best move
     # TODO: Implement search method (Alpha-Beta preferable)\
-
+    possible = filter_chosen(board)
+    wins = filter_wins(possible)
+    print(wins)
 
 
     # print(possible_moves)
